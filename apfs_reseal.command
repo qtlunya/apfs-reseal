qtlunya/apfs-reseal

commit 4335f22671400abf8658ce7229f65bd411f6907f

feat: USB device detection on Linux

apfs_reseal.command
@@ -75,7 +75,12 @@ esac
 
 echo '[*] Waiting for device in recovery or DFU mode'
 while true; do
-    devices=$(system_profiler SPUSBDataType | grep -B1 'Vendor ID: 0x05ac')
+    if [ "$os" = Darwin ]; then
+        devices=$(system_profiler SPUSBDataType | grep -B1 'Vendor ID: 0x05ac')
+    else
+        devices=$(lsusb | grep '05ac:'; cat /sys/bus/usb/devices/*/serial)
+    fi
+
     case $devices in
         *SSHRD_Script*)  # ramdisk
             device=$(remote_cmd "/usr/bin/mgask ProductType | tail -1")
@@ -87,13 +92,13 @@ while true; do
 
             break
             ;;
-        *'Product ID: 0x12a'[8ab]*)  # normal
+        *12a[8ab]*)  # normal
             ideviceenterrecovery "$(idevice_id -l)"
             while ! irecovery -q >/dev/null 2>/dev/null; do
                 sleep 0.1
             done
             ;;
-        *'Product ID: 0x1281'*|*'Product ID: 0x1227'*)  # recovery/DFU
+        *1281*|*1227*)  # recovery/DFU
             palera1n --dfuhelper
             while ! irecovery -q >/dev/null 2>/dev/null; do
                 sleep 0.1

commit 97cb2d42be7fbbfb704a5fc1a25ed99a5e1cabc9

fix: running from ramdisk mode on macOS
This was broken by 4335f22.

apfs_reseal.command
@@ -77,11 +77,13 @@ echo '[*] Waiting for device in recovery or DFU mode'
 while true; do
     if [ "$uname" = Darwin ]; then
         devices=$(system_profiler SPUSBDataType | grep -B1 'Vendor ID: 0x05ac')
+        serials=$(system_profiler SPUSBDataType | grep 'Serial Number')
     else
-        devices=$(lsusb | grep '05ac:'; cat /sys/bus/usb/devices/*/serial)
+        devices=$(lsusb | grep '05ac:')
+        serials=$(cat /sys/bus/usb/devices/*/serial)
     fi
 
-    case $devices in
+    case $serials in
         *SSHRD_Script*)  # ramdisk
             device=$(remote_cmd "/usr/bin/mgask ProductType | tail -1")
             boardconfig=$(remote_cmd "/usr/bin/mgask HWModelStr | tail -1" | tr '[:upper:]' '[:lower:]')
@@ -92,6 +94,9 @@ while true; do
 
             break
             ;;
+    esac
+
+    case $devices in
         *12a[8ab]*)  # normal
             ideviceenterrecovery "$(idevice_id -l)"
             while ! irecovery -q >/dev/null 2>/dev/null; do
@@ -133,6 +138,7 @@ while true; do
             break
             ;;
     esac
+
     sleep 1
 done
 